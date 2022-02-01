a2910b68b79b34b026a15855bacfa3cb831c83ed
kramstyles <mark.eke@decagon.dev>
2022-02-01 19:41:51 +0100

    Finised prime.py

---
+++ prime.py
@@ -0,0 +1,34 @@
+def check_prime(number):
+    """
+    This function checks if a number is a prime number
+    param: number(int) - Inputted number by the user
+    return: boolean(true / false) - returns true if prime, false if not prime
+    """
+
+    if type(number) != int and type(number) != float:
+        ans = "Only number inputs allowed"
+    else:
+        ans = True
+        if number <= 1 and number % 2 == 0 and number != 2:
+            ans = False
+        else:    
+            for x in range (2, number):
+                if number % x == 0:
+                    ans = False
+
+    if ans:
+        ans = f"{number} is a prime number"
+    else:
+        factors = []
+        for x in range (1, number):
+            if not number % x:
+                factors.append(x)
+        ans = f"{number} is not a prime number. {factors}"
+
+    return ans
+                
+    
+
+    
+
+print(check_prime(100))
